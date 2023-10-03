07aac12d098902325df68013033d6c8664499663
afernandez <afernandez@106-11.info2>
2023-10-03 14:41:27 +0200

    Finished exercise 5 and added exercise 6

--- src/ejercicio5/Ejercicio5.java
+++ src/ejercicio5/Ejercicio5.java
@@ -18,9 +18,10 @@ public class Ejercicio5 {
 		System.out.println("Introduce el tercer número:");
 		num[2] = sc.nextInt();
 		
+		
 		for(int i = 0; i<num.length;i++) {
-			for(int j = i+1; j<num.length-1;j++) {
-				if(num[i] > num[j]) {
+			for(int j = i+1; j<num.length;j++) {
+				if(num[i] < num[j]) {
 					temp = num[i];
 					num[i] = num[j];
 					num[j] = temp;
